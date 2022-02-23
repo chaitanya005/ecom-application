09a8fdb7ca7559620613e4a2dba2fd6c7a65dd58
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-02-23 16:30:26 +0530

    Update: changed baseUrl to production

--- src/utils/ajax-helper.js
+++ src/utils/ajax-helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://stark-shelf-91723.herokuapp.com/api/",
 });
 
 export default instance;
